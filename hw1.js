bcd715c19e0cd19c64ba23db200a6eefd4344ae5
smarble2 <smarble2@umbc.edu>
2024-05-29 17:04:04 -0400

    Minor title update

--- hw1.js
+++ hw1.js
@@ -38,7 +38,7 @@ function homeFunction() {
     document.body.style.backgroundColor = 'rgba(173,216,230,1)';
     //var and message
     var homeMsg = document.getElementById("homeMessage");
-    homeMsg.innerText = "Welcome to Shianne's 447 Homework!\n This is the Home Page🏠 \nPlease click About Me tab to learn more about me";
+    homeMsg.innerText = "Welcome to Shianne's Website!\n This is the Home Page🏠 \nPlease click About Me tab to learn more about me";
     //remove messages from about me page
     var msg = document.getElementById("message");
     var msg2 = document.getElementById("message2");
